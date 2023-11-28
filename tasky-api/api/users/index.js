0d1d59074cb2b5b0b79cddfdccabbe574ccb4940
Alex Fan <20077347@mail.wit.ie>
2023-11-28 14:08:16 +0000

    Mongo lab Exercises done

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -3,6 +3,22 @@ import User from './userModel';
 
 const router = express.Router(); // eslint-disable-line
 
+// Custom password validation middleware
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  // Define the password validation regular expression
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  // Check if the password meets the requirements
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({ error: 'Password must be at least 8 characters long and contain at least one letter, one digit, and one special character.' });
+  }
+
+  // If the password is valid, continue with the next middleware or route handler
+  next();
+};
+
 // Get all users
 router.get('/', async (req, res) => {
     const users = await User.find();
@@ -10,13 +26,15 @@ router.get('/', async (req, res) => {
 });
 
 // register(Create)/Authenticate User
-router.post('/', async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
+router.post('/', validatePassword, async (req, res) => {
+    if (req.query.action === 'register') {
+      // Hash the password before saving it to the database
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      await User({ ...req.body, password: hashedPassword }).save();
+      res.status(201).json({
+        code: 201,
+        msg: 'Successfully created a new user.',
+      });
     }
     else {  //Must be an authenticate then!!! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
